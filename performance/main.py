918d10c2e8ad75957f3c6a22bc7d2034dfaf11d6
Michael Burbank <burbank2@yahoo.com>
2025-11-20 14:02:55 -0700

    Create main.py

---
+++ performance/main.py
@@ -0,0 +1,16 @@
+# main.py
+# MVC pattern: Model-View-Controller
+# Main python controller for the project
+
+# Imported modules
+import performance_function
+import schedule_function
+
+
+def main():
+    performance_function.performance_function()
+    schedule_function.schedule_function()
+
+
+if __name__ == "__main__":
+    main()
