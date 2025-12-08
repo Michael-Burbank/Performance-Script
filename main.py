bd4f514ec11aae0d7970696f0469c03d2e0a54e0
Michael Burbank <burbank2@yahoo.com>
2025-12-07 18:58:09 -0700

    Modify function names. Create script to check the health and status of a web server.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+# main.py
+# MVC pattern: Model-View-Controller
+# Main python controller for the project
+
 # Imported modules
 import http_health_check
 import scheduler
 
 
-def main():
-    http_health_check.check_http_health()
-    scheduler.schedule_health_checks()
 
-    
+def main():
+        http_health_check.check_http_health()
+        scheduler.schedule_health_check()
+
+
 if __name__ == "__main__":
     main()
--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,17 @@
-# Import libraries
-import schedule
 import time
+import schedule
+# import time
 import http_health_check
 
+def schedule_health_check():
+    try:
+        # Schedule the function to run every 60 min
+        schedule.every(60).minutes.do(http_health_check.check_http_health)
 
-def schedule_health_checks():
-    # Schedule the function to run every 60 min
-    schedule.every(60).minutes.do(http_health_check.check_http_health)
+        print("Scheduler running: Press Ctrl+C to exit.")
     
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-
-
+        while True:
+            schedule.run_pending()
+            time.sleep(60)
+    except KeyboardInterrupt:
+        print("Scheduler stopped by user.")
